90cbcf989b52e9f9f3b58fe455c76614ace41bf4
NavyHubb <sngjae972@gmail.com>
2022-08-30 18:34:32 +0900

    /api/upload 경로의 인증 생략 부분 제거

--- src/main/java/com/example/intermediate/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/intermediate/configuration/SecurityConfiguration.java
@@ -58,7 +58,6 @@ public class SecurityConfiguration {
         .antMatchers("/api/member/**").permitAll()
         .antMatchers("/api/post/**").permitAll()
         .antMatchers("/api/comment/**").permitAll()
-        .antMatchers("/api/upload").permitAll()
         .anyRequest().authenticated()
 
         .and()
