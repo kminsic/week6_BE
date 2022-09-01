c16ed7f4468dae86b0ed49f30bc6c67007ec8c59
NavyHubb <sngjae972@gmail.com>
2022-09-01 10:32:35 +0900

    불필요 파일 삭제 (FileUploadRepository)

--- src/main/java/com/example/intermediate/repository/FileUploadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.intermediate.repository;
-
-import com.example.intermediate.domain.FileUpload;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileUploadRepository extends JpaRepository<FileUpload, Long> {
-
-}
